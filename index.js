32b8cad696f0cee5c82b01ead03e8fb9b9e1c01e
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-22 17:24:46 -0500

    Practiced adding middleware to specific routers

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const PORT = 3000;
 
 const shelterRoutes = require('./routes/shelters');
 const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin');
+
 
 app.use('/shelters', shelterRoutes)
 app.use('/dogs', dogRoutes)
+app.use('/admin', adminRoutes)
 
 app.listen(PORT, () => {
     console.log(`Connected on port ${PORT}`)
